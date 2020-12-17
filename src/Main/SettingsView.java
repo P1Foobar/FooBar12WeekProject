6226c90365179e580f21de985aba2237ac394094
Andrew <Andrewhtjoseph@gmail.com>
2020-12-16 16:54:31 -0800

    Settings being set up 2

--- src/Main/SettingsView.java
+++ src/Main/SettingsView.java
@@ -2,13 +2,14 @@ package Main;
 
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class SettingsView extends JFrame {
 
-    JSlider population = new JSlider();
-    JSlider infectionRate = new JSlider();
-    JSlider immunePeople = new JSlider();
+
+
+
 
 
     JButton startSimulation = new JButton();
@@ -19,17 +20,37 @@ public SettingsView() {
 
     getContentPane().setBackground(Color.lightGray);
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    setBounds(0, 0, 300, 800);
+    setBounds(0, 0, 400, 800);
 
-   population.setBounds(20, 0, 260, 50);
+
+    JSlider population = new JSlider(0, 100, 1);
+    population.setMajorTickSpacing(10);
+    population.setMinorTickSpacing(1);
+    population.setPaintTicks(true);
+    population.setPaintLabels(true);
+    population.setBounds(0, 0, 400, 50);
     getContentPane().add(population);
 
+    JSlider infectionRate = new JSlider(0, 100, 1);
+    infectionRate.setMajorTickSpacing(10);
+    infectionRate.setMinorTickSpacing(1);
+    infectionRate.setPaintTicks(true);
+    infectionRate.setPaintLabels(true);
+    infectionRate.setBounds(0, 70, 400, 50);
+   getContentPane().add(infectionRate);
+
+  // getContentPane().add(infectionRate, BorderLayout.NORTH);
 
 
-    getContentPane().add(infectionRate);
-
-
+    JSlider immunePeople = new JSlider(0, 100, 1);
+    immunePeople.setMajorTickSpacing(10);
+    immunePeople.setMinorTickSpacing(1);
+    immunePeople.setPaintTicks(true);
+    immunePeople.setPaintLabels(true);
+    immunePeople.setBounds(0, 0, 20, 50);
     getContentPane().add(immunePeople);
+
+
 
 }
 
