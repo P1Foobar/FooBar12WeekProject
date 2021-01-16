c352c95465bb1a2a36f4692ec82241f38a1423e6
davidramsayer <70966199+davidramsayer@users.noreply.github.com>
2021-01-15 22:50:06 -0800

    ticket work

---
+++ src/SimulationItems/SimObject.java
@@ -0,0 +1,113 @@
+package SimulationItems;
+
+import java.util.Random;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.awt.Color;
+
+abstract public class SimObject implements Runnable {
+
+    protected Area grid;
+    protected int rowp;
+    protected int colp;
+
+    private boolean isPaused;
+
+    private Random random;
+
+    static final int TURN_DELAY = 500;
+    static final int TURN_RANDOM = 200;
+
+    public SimObject ()
+    {
+        random = new Random();
+    }
+
+    final public void init (int row, int col, Area grid)
+    {
+        this.rowp = row;
+        this.colp = col;
+        this.grid = grid;
+        this.isPaused = false;
+    }
+
+    public void run ()
+    {
+        isPaused = false;
+
+        while (true) {
+            if (!isPaused) {
+                executeTurn ();
+                delay (TURN_DELAY + random.nextInt(TURN_RANDOM));
+            }
+        }
+    }
+
+    public void resumeObject()
+    {
+        isPaused = false;
+    }
+
+    public void pauseObject()
+    {
+        isPaused = true;
+    }
+
+    public boolean isPaused()
+    {
+        return isPaused;
+    }
+
+    abstract public void executeTurn();
+
+    public Color getColor ()
+    {
+        return Color.green;
+    }
+
+    final public int getRow ()
+    {
+        return rowp;
+    }
+
+    final public void delay (int ms)
+    {
+        try {
+            Thread.sleep (ms);
+        } catch (InterruptedException ie) {
+            ;
+        }
+    }
+
+    final public int getColumn ()
+    {
+        return colp;
+    }
+
+    synchronized public Enumeration getNeighbors () {
+
+        Vector objects = new Vector ();
+
+        for (int row = rowp - 1; row <= rowp + 1; row++) {
+            for (int col = colp - 1; col <= colp + 1; col++) {
+                if (row != rowp || col != colp) {
+                    if (grid.validLocation (row, col)) {
+                        SimObject obj = grid.getObjectAt (row, col);
+
+                        if (obj != null) {
+                            objects.addElement (obj);
+                        }
+                    }
+                }
+            }
+        }
+
+        return objects.elements ();
+    }
+
+    final public Area getGrid ()
+    {
+        return grid;
+    }
+
+}
